40be254d8361ad3923d00c186fd238e86ee96299
iseau395 <70857830+iseau395@users.noreply.github.com>
2024-04-05 17:44:14 -0700

    fix parameter structs

--- include/lib15442c/chasis/drive_controller.hpp
+++ include/lib15442c/chasis/drive_controller.hpp
@@ -3,8 +3,140 @@
 #include "lib15442c/controller/pid.hpp"
 #include "lib15442c/math/pose.hpp"
 
-namespace lib15442c {
-    class DriveController {
+namespace lib15442c
+{
+    /**
+     * @brief The parameters for the turning functions
+     */
+    struct AngleParameters
+    {
+        /**
+         * @brief The radius of the curved path to follow, 0 to turn in place
+         *
+         */
+        double arc_radius = 0;
+
+        /**
+         * @brief The threshold which error need to be in to end
+         *
+         */
+        double threshold = 0.75;
+
+        /**
+         * @brief The timeout in case the move takes too long (ms)
+         */
+        double timeout = 1000;
+
+        /**
+         * @brief The maximum speed
+         */
+        double max_speed = 127;
+        /**
+         * @brief The minimum speed, defaults to the one set by the DriveController constructor
+         */
+        double min_speed = -1;
+
+        /**
+         * @brief Whether to use the chain exit condition instead of the normal one
+         */
+        bool chained = false;
+        /**
+         * @brief Whether to run asynchronously
+         */
+        bool async = false;
+    };
+
+    /**
+     * @brief The parameters for the drive function
+     */
+    struct DriveParameters
+    {
+        /**
+         * @brief The angle to drive at to keep straight, defaults to the current angle
+         */
+        lib15442c::Angle angle = lib15442c::Angle::none();
+
+        /**
+         * @brief The threshold which error must be in to end
+         */
+        double threshold = 0.1;
+
+        /**
+         * @brief The timeout in case the move takes too long (ms)
+         */
+        double timeout = 1000;
+
+        /**
+         * @brief The maximum speed
+         */
+        double max_speed = 127;
+        /**
+         * @brief The minimum speed, defaults to the one set by the DriveController constructor
+         */
+        double min_speed = -1;
+
+        /**
+         * @brief Whether to use the chain exit condition instead of the normal one
+         */
+        bool chained = false;
+        /**
+         * @brief Whether to run asynchronously
+         */
+        bool async = false;
+    };
+
+    /**
+     * @brief The parameters for the boomerang controller
+     */
+    struct BoomerangParameters
+    {
+        /**
+         * @brief Whether to go in reverse
+         */
+        bool backwards = false;
+        /**
+         * @brief The lead, should be between 0 and 1. Affects how far away the path should curve into the target
+         */
+        double lead = 0.6;
+        /**
+         * @brief How far away the robot should be from the end before the movement ends
+         */
+        double threshold = 1;
+        /**
+         * @brief The distance from the end in which the target point is ignored, and the program focuses on just getting the right angle
+         */
+        double cross_track_threshold = 3;
+        /**
+         * @brief How much to prioritize turning over driving; Outside of this error threshold (degrees), the drive speed is reduced to prioritize turning
+         */
+        double turn_priority = 15;
+
+        /**
+         * @brief The timeout in case the move takes too long (ms)
+         */
+        double timeout = 5;
+
+        /**
+         * @brief The maximum speed
+         */
+        double max_speed = 127;
+        /**
+         * @brief The minimum speed, defaults to the one set by the DriveController constructor
+         */
+        double min_speed = -1;
+
+        /**
+         * @brief Whether to use the chain exit condition instead of the normal one
+         */
+        bool chained = false;
+        /**
+         * @brief Whether to run asynchronously
+         */
+        bool async = false;
+    };
+
+    class DriveController
+    {
     private:
         std::shared_ptr<IDrivetrain> drivetrain;
         std::shared_ptr<IOdometry> odometry;
@@ -21,51 +153,13 @@ namespace lib15442c {
             std::shared_ptr<IDrivetrain> drivetrain,
             std::shared_ptr<IOdometry> odometry,
             std::shared_ptr<PID> drive_pid, std::shared_ptr<PID> turn_pid,
-            double default_min_speed = 12
-        ) : drivetrain(drivetrain), odometry(odometry), drive_pid(drive_pid), turn_pid(turn_pid), default_min_speed(default_min_speed) {};
+            double default_min_speed = 12) : drivetrain(drivetrain), odometry(odometry), drive_pid(drive_pid), turn_pid(turn_pid), default_min_speed(default_min_speed){};
 
         // turn functions
 
-        struct AngleParameters {
-            /**
-             * @brief The radius of the curved path to follow, 0 to turn in place
-             * 
-             */
-            double arc_radius = 0;
-
-            /**
-             * @brief The threshold which error need to be in to end
-             * 
-             */
-            double threshold = 0.75;
-
-            /**
-             * @brief The timeout in case the move takes too long (ms)
-             */
-            double timeout = 1000;
-
-            /**
-             * @brief The maximum speed
-             */
-            double max_speed = 127;
-            /**
-             * @brief The minimum speed, defaults to the one set by the DriveController constructor
-             */
-            double min_speed = -1;
-
-            /**
-             * @brief Whether to use the chain exit condition instead of the normal one
-             */
-            bool chained = false;
-            /**
-             * @brief Whether to run asynchronously
-             */
-            bool async = false;
-        };
-
         /**
          * @brief Turn a certain amount relative to the robot's current rotation
-         * 
+         *
          * @param angle The angle to end at
          * @param parameters Any extra parameters
          */
@@ -80,7 +174,7 @@ namespace lib15442c {
 
         /**
          * @brief Face a point in x-y space
-         * 
+         *
          * @param pos The position to face
          * @param angle_offset An amount to offset the faced angle from the target point
          * @param parameters Any extra parameters
@@ -89,102 +183,18 @@ namespace lib15442c {
 
         // drive functions
 
-        struct DriveParameters {
-            /**
-             * @brief The angle to drive at to keep straight, defaults to the current angle
-             */
-            lib15442c::Angle angle = lib15442c::Angle::none();
-
-            /**
-             * @brief The threshold which error must be in to end
-             */
-            double threshold = 0.1;
-
-            /**
-             * @brief The timeout in case the move takes too long (ms)
-             */
-            double timeout = 1000;
-
-            /**
-             * @brief The maximum speed
-             */
-            double max_speed = 127;
-            /**
-             * @brief The minimum speed, defaults to the one set by the DriveController constructor
-             */
-            double min_speed = -1;
-
-            /**
-             * @brief Whether to use the chain exit condition instead of the normal one
-             */
-            bool chained = false;
-            /**
-             * @brief Whether to run asynchronously
-             */
-            bool async = false;
-        };
-
         /**
          * @brief Drive a distance using the drive PID
-         * 
+         *
          * @param distance The distance to drive (inches)
          * @param parameters Any extra parameters
          */
         void drive(double distance, DriveParameters parameters = {});
 
-        /**
-         * @brief The parameters for the boomerang controller
-         */
-        struct BoomerangParameters {
-            /**
-             * @brief Whether to go in reverse
-             */
-            bool backwards = false;
-            /**
-             * @brief The lead, should be between 0 and 1. Affects how far away the path should curve into the target
-             */
-            double lead = 0.6;
-            /**
-             * @brief How far away the robot should be from the end before the movement ends
-             */
-            double threshold = 1;
-            /**
-             * @brief The distance from the end in which the target point is ignored, and the program focuses on just getting the right angle
-             */
-            double cross_track_threshold = 3;
-            /**
-             * @brief How much to prioritize turning over driving; Outside of this error threshold (degrees), the drive speed is reduced to prioritize turning
-             */
-            double turn_priority = 15;
-
-            /**
-             * @brief The timeout in case the move takes too long (ms)
-             */
-            double timeout = 5;
-
-            /**
-             * @brief The maximum speed
-             */
-            double max_speed = 127;
-            /**
-             * @brief The minimum speed, defaults to the one set by the DriveController constructor
-             */
-            double min_speed = -1;
-
-            /**
-             * @brief Whether to use the chain exit condition instead of the normal one
-             */
-            bool chained = false;
-            /**
-             * @brief Whether to run asynchronously
-             */
-            bool async = false;
-        };
-
         void boomerang(lib15442c::Pose pos, BoomerangParameters parameters = {});
 
         // async functions
-        
+
         /**
          * @brief Whether the robot is done moving
          *
