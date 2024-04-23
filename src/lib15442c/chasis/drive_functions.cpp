48dbec527250de11b82f2c6e80e6096348ba9697
iseau395 <70857830+iseau395@users.noreply.github.com>
2024-04-23 13:53:17 -0700

    move drive and turn functions into their own files

---
+++ src/lib15442c/chasis/drive_functions.cpp
@@ -0,0 +1,132 @@
+#include "drive_controller.hpp"
+#include "lib15442c/math/math.hpp"
+#include "lib15442c/logger.hpp"
+
+#include <algorithm>
+
+#define LOGGER "Drive Functions"
+
+void lib15442c::DriveController::drive(double distance, DriveParameters parameters)
+{
+    if (parameters.async)
+    {
+        parameters.async = false;
+
+        pros::Task([this, distance, parameters]
+                   { drive(distance, parameters); });
+        pros::delay(10); // Give the task time to start
+        return;
+    }
+    async_mutex.lock();
+
+    INFO("Start Drive... (%f in)", distance);
+
+    drive_pid->reset();
+    turn_pid->reset();
+
+    Pose starting_position = odometry->getPose();
+
+    double time_correct = 0;
+    double total_error = 0;
+
+    int starting_time = pros::millis();
+
+    Angle target_rotation;
+    if (parameters.angle.is_none())
+    {
+        target_rotation = odometry->getRotation();
+    } else {
+        target_rotation = parameters.angle;
+    }
+
+    auto comp_status = pros::competition::get_status();
+
+    while (pros::competition::get_status() == comp_status)
+    {
+        // double left = left_motors->get_position() - left_start;
+        // double right = right_motors->get_position() - right_start;
+        // double parallel = parallel_tracker->get_position() / 100.0;
+        // double parallelDistance = (parallel - starting_parallel) * degrees_per_inch;
+
+        double distanceTraveled = starting_position.vec().distance_to(odometry->getPosition());
+        double error = distance - (distanceTraveled * lib15442c::sgn(distance));
+
+        double speed = drive_pid->calculateError(error);
+
+        speed = std::min(fabs(speed), parameters.max_speed) * lib15442c::sgn(speed);
+        speed = std::max(fabs(speed), parameters.min_speed) * lib15442c::sgn(speed);
+
+        if (fabs(speed) < 40)
+        {
+            total_error += error;
+        }
+
+        speed += std::min(fabs(total_error) * 0.34, 30.0) * lib15442c::sgn(error);
+
+        Angle angle_error = target_rotation.error_from(odometry->getRotation());
+        double rot_speed = turn_pid->calculateError(angle_error.deg());
+
+        // std::cout << pros::millis() << ", " << error << ", " << speed << std::endl;
+
+        drivetrain->move(speed, rot_speed);
+
+        if (!parameters.chained && fabs(error) < parameters.threshold)
+        {
+            time_correct += 20;
+            break;
+        }
+        else
+        {
+            time_correct = 0;
+        }
+
+        if (time_correct >= 40 || (parameters.chained && fabs(distanceTraveled) >= fabs(distance) - parameters.threshold))
+        {
+            break;
+        }
+
+        if ((int)pros::millis() - starting_time >= parameters.threshold)
+        {
+            WARN_TEXT("drive timed out!");
+            break;
+        }
+
+        pros::delay(20);
+    }
+
+    if (!parameters.chained)
+    {
+        drivetrain->move(0, 0);
+    }
+    async_mutex.unlock();
+
+    INFO_TEXT("End Drive!");
+}
+
+void lib15442c::DriveController::drive_time(double voltage, double time, DriveTimeParameters parameters) {
+    double start_time = pros::millis();
+
+    turn_pid->reset();
+
+    while (pros::millis() - start_time < time) {
+        double current_time = pros::millis() - start_time;
+
+        double ramp_up_voltage = parameters.ramp_up ? parameters.ramp_speed * current_time : voltage;
+        double ramp_down_voltage = parameters.ramp_down ? -parameters.ramp_speed * (current_time - time) : voltage;
+        double drive_speed = std::min(ramp_up_voltage, std::min(voltage, ramp_down_voltage));
+
+        double turn_speed = 0;
+
+        if (!parameters.angle.is_none()) {
+            Angle error = odometry->getRotation().error_from(parameters.angle);
+
+            turn_speed = turn_pid->calculateError(error.deg());
+        }
+
+        drivetrain->move(drive_speed, turn_speed);
+
+        pros::delay(10);
+    }
+
+    drivetrain->move(0, 0);
+}
---
+++ src/lib15442c/chasis/turn_functions.cpp
@@ -0,0 +1,152 @@
+#include "drive_controller.hpp"
+#include "lib15442c/math/math.hpp"
+#include "lib15442c/logger.hpp"
+
+#include <algorithm>
+
+#define LOGGER "Turn Functions"
+
+void lib15442c::DriveController::turn(lib15442c::Angle angle, AngleParameters parameters)
+{
+    INFO("Start turn... (%f deg)", angle.deg());
+
+    Angle global_angle = odometry->getRotation() + angle;
+
+    face(FaceAngleTarget { angle: global_angle }, parameters);
+    
+    INFO_TEXT("End turn!");
+}
+
+void lib15442c::DriveController::faceAngle(lib15442c::Angle angle, AngleParameters parameters)
+{
+    INFO("Start face angle... (%f deg)", angle.deg());
+
+    face(FaceAngleTarget { angle });
+    
+    INFO_TEXT("End face angle!");
+}
+
+void lib15442c::DriveController::facePoint(lib15442c::Pose point, lib15442c::Angle angle_offset, AngleParameters parameters)
+{
+    INFO("Start face point... (%f, %f)", point.x, point.y);
+
+    face(FacePointTarget { point, angle_offset }, parameters);
+    
+    INFO_TEXT("End face point!");
+}
+
+void lib15442c::DriveController::face(FaceTarget target, AngleParameters parameters) {
+    if (parameters.async)
+    {
+        parameters.async = false;
+
+        pros::Task([this, target, parameters]
+                   { face(target, parameters); });
+        pros::delay(10); // Give the task time to start
+        return;
+    }
+    async_mutex.lock();
+
+    Angle target_angle;
+
+    // Calculate target angle if facing a point
+    if (FacePointTarget *point_target = std::get_if<FacePointTarget>(&target))
+    {
+        target_angle = odometry->getPosition().angle_to(point_target->pos.vec());
+        if (point_target->pos.y - odometry->getY() < 0)
+        {
+            target_angle += 180_deg;
+        }
+        target_angle += point_target->angle_offset;
+    } else {
+        FaceAngleTarget *angle_target = std::get_if<FaceAngleTarget>(&target);
+
+        target_angle = angle_target->angle;
+    }
+
+    // Calculate ratios
+    Angle start_rotation = odometry->getRotation();
+    Angle initial_error = start_rotation.error_from(target_angle);
+
+    double left_arc_distance = (parameters.arc_radius + drivetrain->get_track_width() / 2.0) * initial_error.rad();
+    double right_arc_distance = (parameters.arc_radius - drivetrain->get_track_width() / 2.0) * initial_error.rad();
+
+    double left_ratio = lib15442c::sgn(left_arc_distance) * fabs(std::max(std::min(left_arc_distance / right_arc_distance, 1.0), -1.0));
+    double right_ratio = lib15442c::sgn(right_arc_distance) * fabs(std::max(std::min(right_arc_distance / left_arc_distance, 1.0), -1.0));
+
+    if (parameters.arc_radius == 0)
+    {
+        left_ratio = 1;
+        right_ratio = -1;
+    }
+
+    turn_pid->reset();
+    int time_correct = 0;
+
+    int starting_time = pros::millis();
+
+    auto comp_status = pros::competition::get_status();
+
+    while (pros::competition::get_status() == comp_status)
+    {
+        // Calculate target angle if facing a point
+        if (FacePointTarget *point_target = std::get_if<FacePointTarget>(&target))
+        {
+            target_angle = odometry->getPosition().angle_to(point_target->pos.vec());
+            if (point_target->pos.y - odometry->getY() < 0)
+            {
+                target_angle += 180_deg;
+            }
+            target_angle += point_target->angle_offset;
+        }
+
+        Angle rotation = odometry->getRotation();
+        Angle error = rotation.error_from(target_angle);
+
+        if (fabs(error.deg()) < parameters.threshold.deg())
+        {
+            time_correct += 20;
+        }
+        else
+        {
+            time_correct = 0;
+        }
+
+        bool chainCondition = parameters.chained && (sgn(error.deg()) != sgn(initial_error.deg()) || fabs(error.deg()) < 10);
+        if (time_correct >= 100 || chainCondition)
+        {
+            // std::cout << pros::millis() << ", " << rotation << ", " << targetAngle << ", " << error << std::endl;
+            break;
+        }
+
+        if ((int)pros::millis() - starting_time >= parameters.timeout)
+        {
+            WARN_TEXT("Face timed out!");
+            break;
+        }
+
+        double rot_speed = turn_pid->calculateError(error.deg());
+
+        rot_speed = std::max(parameters.min_speed, (double)fabs(rot_speed)) * lib15442c::sgn(rot_speed);
+        rot_speed = std::min(parameters.max_speed, (double)fabs(rot_speed)) * lib15442c::sgn(rot_speed);
+        if (fabs(error.deg()) < 2.5)
+        {
+            rot_speed = parameters.min_speed * lib15442c::sgn(error.deg());
+        }
+
+        // std::cout << pros::millis() << ", " << rot_speed << ", " << error << std::endl;
+
+        double leftSpeed = rot_speed * left_ratio;
+        double rightSpeed = rot_speed * right_ratio;
+
+        drivetrain->tank(leftSpeed, rightSpeed);
+
+        pros::delay(20);
+    }
+
+    if (!parameters.chained)
+    {
+        drivetrain->move(0, 0);
+    }
+    async_mutex.unlock();
+}
